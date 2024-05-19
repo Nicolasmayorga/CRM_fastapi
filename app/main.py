17a8987289d3e3527badf4fe3ea641914cfdd699
Nicolas Mayorga <nmayorgav@gmail.com>
2024-05-18 23:41:32 -0600

    creation of the user models

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.core.database import engine, Base
+from app.models import user
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+)
+
+# Crear tablas
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the CRM system"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
